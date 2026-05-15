99035320aa5317aa3e2029e139af2d4514913f3e
Gustavo Coelho <coelho1914@gmail.com>
2026-05-15 15:07:13 -0300

    Add comentario

--- functions.py
+++ functions.py
@@ -2,11 +2,14 @@
 def soma(a, b):
     return a + b
 
+#função de subtração
 def subtrai(a, b):
     return a - b
 
+# função de divisão
 def divisao(a, b):
     if b !=0:
         return a/b
     
     Exception("Não pode dividir por zero")
+
